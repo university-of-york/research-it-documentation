0f4b215e06876ae2d98c47a90eb4a21175653cff
Neil Douglas <neil.douglas@york.ac.uk>
2023-10-26 10:59:41 +0100

    updated module names of all I could

--- docs/source/replacements.py
+++ docs/source/replacements.py
@@ -20,8 +20,8 @@ variable_replacements = {
     "{SHEBANG}" : "#!/usr/bin/env bash",  # used in all the example jobscripts
 
     # modules and versions used in the various jobscript examples for easy updating & consistency
-    "{MOD_PYTHON}": "Python/3.10.8-GCCcore-12.2.0",
-    "{MOD_COMPILER}": "GCC/12.2.0",
+    "{MOD_PYTHON}": "Python/3.11.3-GCCcore-12.3.0",
+    "{MOD_COMPILER}": "GCC/12.3.0",
     "{MOD_R}": "R/4.2.1-foss-2022a",
     "{MOD_AMBER_CPU}": "Amber/22.3-foss-2022b-AmberTools-23.0",
     "{MOD_AMBER_GPU}": "Amber/16-foss-2018a-AmberTools-17-CUDA",
@@ -30,16 +30,16 @@ variable_replacements = {
     "{MOD_MONGODB}": "MongoDB/4.2.3",
     "{MOD_VOXFE}": "VOX-FE/2.0.1-foss-2017b",
     "{MOD_RELION}": "RELION/4.0.1-foss-2021a",
-    "{MOD_ALPHAFOLD_CPU}": "AlphaFold/2.1.2-foss-2021a",
+    "{MOD_ALPHAFOLD_CPU}": "AlphaFold/2.3.1-foss-2022a",
     "{MOD_ALPHAFOLD_GPU}": "AlphaFold/2.3.1-foss-2022a-CUDA-11.7.0",
     "{ALPHAFOLD_DB_PATH}": "/mnt/bb/striped/alphafold_db/",
     "{APLHPFOLD_DB_DATE}": "20230518",
-    "{MOD_VASP}": "VASP/5.4.4-intel-2019a",
-    "{MOD_TOOLCHAIN_FOSS}": "foss/2022b",  # used in the AtChem2 example
-    "{MOD_CMAKE}": "CMake/3.24.3-GCCcore-12.2.0",  # used in the AtChem2 example
+    "{MOD_VASP}": "VASP/5.4.4-intel-2023a",
+    "{MOD_TOOLCHAIN_FOSS}": "foss/2023a",  # used in the AtChem2 example
+    "{MOD_CMAKE}": "CMake/3.26.3-GCCcore-12.3.0",  # used in the AtChem2 example
     "{MOD_APPTAINER}": "Singularity/3.5.3",  # has own page
     "{NAME_APPTAINER}": "singularity",
-    "{MOD_MINICONDA}": "lang/Miniconda3",
+    "{MOD_MINICONDA}": "Miniconda3/23.5.2-0",
 
 }
 
